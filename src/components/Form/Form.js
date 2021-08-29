7e368e5e6f9d3026b67a4a97fd4805d2152d1c86
Md.Muktar Ali <muktar15-2298@diu.edu.bd>
2021-08-30 03:08:29 +0600

    Form Project Completed

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 const Form = () => {
+  const [input, setInput] = useState({
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [showInput, setShowInput] = useState({});
+
+  const handleInput = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    setInput((preValue) => {
+      return {
+        ...preValue,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowInput(input);
+  };
   return (
     <div className="form-container">
       <div className="intro-container half-width">
+        <p>{showInput.name}</p>
+        <p>{showInput.username}</p>
+        <p>{showInput.email}</p>
+        <p>{showInput.password}</p>
         <h5>Join With Me</h5>
         <p>Subscribe My Github Site for next update</p>
         <button className="intro-btn">About Me</button>
@@ -12,17 +40,41 @@ const Form = () => {
         <div className="form">
           <h2>Register Here</h2>
           <br />
-          <form>
-            <input type="text" name="name" placeholder="   Name" />
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="name"
+              placeholder="   Name"
+              value={input.name}
+              onChange={handleInput}
+            />
             <br />
             <br />
-            <input type="text" name="username" placeholder="   Username" />
+            <input
+              type="text"
+              name="username"
+              placeholder="   Username"
+              value={input.username}
+              onChange={handleInput}
+            />
             <br />
             <br />
-            <input type="email" name="email" placeholder="   Email" />
+            <input
+              type="email"
+              name="email"
+              placeholder="   Email"
+              value={input.email}
+              onChange={handleInput}
+            />
             <br />
             <br />
-            <input type="password" name="password" placeholder="   Password" />
+            <input
+              type="password"
+              name="password"
+              placeholder="   Password"
+              value={input.password}
+              onChange={handleInput}
+            />
             <br />
             <br />
             <button type="submit" className="btn">
